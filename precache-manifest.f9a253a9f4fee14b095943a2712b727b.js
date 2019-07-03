c8d1944af8d29739015d06f384964c3dad5f7e7a
quicknotesV8 <julianbaeza202@gmail.com>
2019-07-03 22:45:38 +1000

    Updates

---
+++ precache-manifest.f9a253a9f4fee14b095943a2712b727b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc8462ee5a0ca28d20d76084122b60dd",
+    "url": "/QuickNote/index.html"
+  },
+  {
+    "revision": "5c12e06b2a02ed10b141",
+    "url": "/QuickNote/static/css/main.e2809bdf.chunk.css"
+  },
+  {
+    "revision": "25833170af67bb366e72",
+    "url": "/QuickNote/static/js/2.36407158.chunk.js"
+  },
+  {
+    "revision": "5c12e06b2a02ed10b141",
+    "url": "/QuickNote/static/js/main.6d959efb.chunk.js"
+  },
+  {
+    "revision": "a3bd3af78363ef10e59d",
+    "url": "/QuickNote/static/js/runtime~main.aaed5410.js"
+  }
+]);
